2386cb116f23f2454a34fead5dcb10491eb6db37
chaitanyachormale <155938538+chaitanyachormale@users.noreply.github.com>
2026-02-15 23:07:25 +0530

    Add files via upload

---
+++ Basic.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Basic{
+      public static void main(String arg[] ){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        System.out.print(a);
+
+
+       
+ 
+
+
+     }
+}
